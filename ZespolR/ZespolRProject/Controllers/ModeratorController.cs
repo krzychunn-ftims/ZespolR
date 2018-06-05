4fe5aee6b20ec56a2e3bf990b5436c18003e36af
Majos <221625@edu.p.lodz.pl>
2018-06-06 01:30:12 +0200

    Added example of generating views and methods for Editor

--- ZespolR/ZespolRProject/Controllers/ModeratorController.cs
+++ ZespolR/ZespolRProject/Controllers/ModeratorController.cs
@@ -70,5 +70,28 @@ namespace ZespolRProject.Controllers
 
         }
 
+        public ActionResult Create()
+        {
+            ViewBag.ed_mod = new SelectList(db.Moderator, "mod_id", "mod_name");
+            return View();
+        }
+
+        // POST: Editors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ed_id,ed_name,ed_surname,ed_phone,ed_def_lng,ed_password,ed_mod,ed_isActive")] Editor editor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Editor.Add(editor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ed_mod = new SelectList(db.Moderator, "mod_id", "mod_name", editor.ed_mod);
+            return View(editor);
+        }
     }
 }
