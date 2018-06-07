8bb8d9971f3b88cb0416b844733b1371333fb0a1
Vlad <gembird196@gmail.com>
2018-06-07 18:39:15 +0300

    Ad functions to moderator to change add or delete redaktors

--- ZespolR/ZespolRProject/Controllers/ModeratorController.cs
+++ ZespolR/ZespolRProject/Controllers/ModeratorController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -42,8 +43,8 @@ namespace ZespolRProject.Controllers
         public ActionResult Requests()
         {
 
-            var a = db.Editor.Where(x => x.ed_isActive == null).ToList();
-            return View(db.Editor.Where(x => x.ed_isActive == null).ToList());
+            //var a = db.Editor.Where(x => x.ed_isActive == null).ToList();
+            return View(db.Editor.ToList());
         }
 
         public ActionResult details(int? id)
@@ -64,11 +65,153 @@ namespace ZespolRProject.Controllers
             }
         }
 
+        public ActionResult details2( )
+        {
+
+            return View(db.Editor.Where(x => x.ed_isActive == null).ToList());
+        }
+        //public ActionResult Edit(int? id)
+        //{
+
+        //    {
+        //        if (id == null)
+        //        {
+        //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //        }
+        //        Editor editor = db.Editor.Find(id);
+        //        if (editor == null)
+        //        {
+        //            return HttpNotFound();
+        //        }
+
+        //        return View(editor);
+        //    }
+        //}
+        //public ActionResult Delete(int? id)
+        //{
+
+        //    {
+        //        if (id == null)
+        //        {
+        //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //        }
+        //        Editor editor = db.Editor.Find(id);
+        //        if (editor == null)
+        //        {
+        //            return HttpNotFound();
+        //        }
+
+        //        return View(editor);
+        //    }
+        //}
+
         public ActionResult t1()
         {
             return View(db.Editor.Where(x => x.ed_isActive == null).ToList());
 
         }
 
+        public ActionResult Addnew()
+        {
+            return View();
+        }
+
+        // GET: Editors/Create
+        public ActionResult Create()
+        {
+            ViewBag.ed_mod = new SelectList(db.Moderator, "mod_id", "mod_name");
+            return View();
+        }
+
+        // POST: Editors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ed_id,ed_name,ed_surname,ed_phone,ed_def_lng,ed_password,ed_mod,ed_isActive")] Editor editor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Editor.Add(editor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ed_mod = new SelectList(db.Moderator, "mod_id", "mod_name", editor.ed_mod);
+            return View(editor);
+        }
+
+
+        //GET: Editors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Editor editor = db.Editor.Find(id);
+            if (editor == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ed_mod = new SelectList(db.Moderator, "mod_id", "mod_name", editor.ed_mod);
+            return View(editor);
+        }
+
+        // POST: Editors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ed_id,ed_name,ed_surname,ed_phone,ed_def_lng,ed_password,ed_mod,ed_isActive")] Editor editor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(editor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ed_mod = new SelectList(db.Moderator, "mod_id", "mod_name", editor.ed_mod);
+            return View(editor);
+        }
+
+        // GET: Editors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Editor editor = db.Editor.Find(id);
+            if (editor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(editor);
+        }
+
+        // POST: Editors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Editor editor = db.Editor.Find(id);
+            db.Editor.Remove(editor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
     }
+
+
+
 }
